cb9472be89e7cd4ac3bdeaf7dbd563409aaf72d3
sobitov2000 <90320800+sobitov2000@users.noreply.github.com>
2021-11-18 17:34:45 +0500

    javascriptni o'rganishdagi holat

---
+++ java dars_2/js/main.js
@@ -0,0 +1,48 @@
+// var myBudget = 1000
+// var myExpense = prompt ("Qancha pul ishlatdingiz?")
+// console.log(myBudget - myExpense);
+// Boolean
+// console.log(10 > 9);
+// console.log(10 == 10);
+// console.log("ahmad" !== "ahmad");
+// console.log("9" === 9);
+
+// var x = 10
+// var y = 10
+// if(10 > 10){
+//     console.log("salom");
+// } else {
+//     console.log("hayir");
+// }
+
+// Vazifa
+
+// var myName = prompt ("Ismingizni kiriting?")
+// var name = "ahmad"
+
+// if(name == myName){
+//     console.log("salom aleykum " + name);
+// } else{
+//     console.log("Saolom mehmon");
+// }
+
+// console.log("salom" .toUpperCase());
+
+// var myName = prompt ("Ismingizni kiriting?")
+// myName = myName.toLowerCase()
+// var name = "ahmad"
+
+// if(name == myName){
+//         console.log("saolm aleykum " + name);
+// } else{
+//     console.log("salom mehmon");
+// }
+
+let confirmation = confirm("Sizning 1000$ pulingiz bormi?")
+console.log(confirmation);
+if(confirmation){
+    var rozilik = confirm("Biznes qilamiz?")
+}
+else{
+    alert{"Ishlasangiz bo'lmaydimi"}
+}
